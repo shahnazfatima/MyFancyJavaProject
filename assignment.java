3c292b09bc444e2f366b7e50dba1085e963c4e65
shahnazfatima <sahanaz.fatima000@gmail.com>
2021-09-08 23:04:41 +0600

    I modified some changes

--- assignment.java
+++ assignment.java
@@ -1,5 +1,6 @@
 public class Assignment {
     public static void main(String[] args) {
-      System.out.println("Developers- X: shahnaz fatima poly");
+      System.out.println("Developers- X: shahnaz fatima");
+      System.out.println("I want to become a web developer");
     }
   }
